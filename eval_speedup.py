747ab9f92f4bfeae3f2df90d35714fe101dda89d
Emruur <emreu02@gmail.com>
2026-03-03 16:39:52 +0100

    new experiment

--- eval_speedup.py
+++ eval_speedup.py
@@ -57,7 +57,7 @@ EXPERIMENT_DEFINITIONS = {
     },
     "row_delta": {
         "grid": {
-            "seq_len": [2048],
+            "seq_len": [1024,2048, 3072],
             "delta": [16.0],
             "row_sim": ["euclidean"]
         },
