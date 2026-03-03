d93204edbaa1dd11201e6bb9840646600e9062a2
Emruur <emreu02@gmail.com>
2026-03-03 16:57:22 +0100

    amk

--- eval_all.py
+++ eval_all.py
@@ -44,7 +44,7 @@ EXPERIMENT_DEFINITIONS = {
     "row_delta": {
         "grid": {
             "scale": [0.05], 
-            "delta": [20],
+            "delta": [0,5,10,15,20],
             "row_sim": ["euclidean"]
         },
         "arg_builder": lambda p: [
