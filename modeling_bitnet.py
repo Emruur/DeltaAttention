70230f86507803e897c1635c5311464b800df350
Emruur <emreu02@gmail.com>
2026-03-04 14:14:44 +0100

    sik

--- modeling_bitnet.py
+++ modeling_bitnet.py
@@ -609,7 +609,7 @@ class BitNetAttention(nn.Module):
                 delta_out = torch.matmul(regular_x, delta_y)
 
             # In-place cumsum to save memory
-            delta_out = torch.cumsum_(delta_out, dim=-1)
+            delta_out = delta_out.cumsum_(dim=-1) # Correct in-place method
 
             output = self._patch_hybrid_attention(delta_out, regular_x, regular_y, bsz, seq_len, blk_size)
             
