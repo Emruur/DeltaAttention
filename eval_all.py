11f1a2f01bfe6660a1128b0de27f4f3913e136b9
Emruur <emreu02@gmail.com>
2026-03-06 11:45:37 +0100

    as

--- eval_all.py
+++ eval_all.py
@@ -61,7 +61,7 @@ EXPERIMENT_DEFINITIONS = {
     "nm_delta": {
         "grid": {
             "scale": [0.05], 
-            "delta": [1,2,3]
+            "delta": [0,1,2,3]
         },
         "arg_builder": lambda p: [
             "--scale", str(p["scale"]), 
