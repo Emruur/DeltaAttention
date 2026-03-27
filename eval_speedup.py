7221fa01420dc88c61071b919935cd1b71371fdf
Emruur <emreu02@gmail.com>
2026-03-27 22:27:27 +0100

    hasd

--- eval_speedup.py
+++ eval_speedup.py
@@ -57,7 +57,7 @@ EXPERIMENT_DEFINITIONS = {
     },
     "row_delta": {
         "grid": {
-            "seq_len": [1024],
+            "seq_len": [1024,2048],
             "delta": [15],
             "row_sim": ["euclidean"],
             "divide_to": [1]
