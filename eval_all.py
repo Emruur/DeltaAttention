e28623aae199cbfbd74e35c52a947de9117dc78e
Emruur <emreu02@gmail.com>
2026-03-30 19:38:09 +0200

    eval all div2

--- eval_all.py
+++ eval_all.py
@@ -57,7 +57,7 @@ EXPERIMENT_DEFINITIONS = {
             "scale": [0.05], 
             "delta": [15],
             "row_sim": ["euclidean"],
-            "divide_to": [1]
+            "divide_to": [2]
         },
         "arg_builder": lambda p: [
             "--scale", str(p["scale"]), 
